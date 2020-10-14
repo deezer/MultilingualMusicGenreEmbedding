bf28197e624558b7ee42757ffe910603c35a1b29
eepure <eepure@deezer.com>
2020-10-14 12:55:19 +0200

    added comments

--- tag_translation/judge.py
+++ tag_translation/judge.py
@@ -3,7 +3,12 @@ from sklearn.metrics import roc_auc_score
 
 
 class Judge():
+    """Judge class to compute evaluation metrics"""
     def compute_macro_metrics(ground_truth, pred):
+        """Compute macro roc auc scores
+        :param ground_truth: the ground truth annotations
+        :param pred: the predicted annotations:
+        :return: macro auc"""
         # Check if there are classes not used in this fold and filter them
         mask = np.nonzero(np.sum(ground_truth, axis=0))[0]
         ground_truth = ground_truth[:, mask]
